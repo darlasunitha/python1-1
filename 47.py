be71d99fcaad385758d6765b1e6503464374e554
darlasunitha <42737284+darlasunitha@users.noreply.github.com>
2018-09-22 14:32:50 +0530

    Update 47.py

--- 47.py
+++ 47.py
@@ -1,5 +1,5 @@
-g=int(raw_input())
-list=[int(x) for x in raw_input().split()]
-a=min(list)
-b=max(list)
-print a,b
+k=int(raw_input())
+list=[int(i) for i in raw_input().split()]
+n=min(list)
+m=max(list)
+print n,m
